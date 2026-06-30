9ab869c63fe71b377e16738af63a2c0b8d5ad5c6
afreen1020sana-art <afreen1020sana@gmail.com>
2026-06-30 13:18:48 +0530

    Update generate_customers.py

--- data/generate_customers.py
+++ data/generate_customers.py
@@ -1,1 +1,24 @@
+from faker import Faker
+import pandas as pd
+import random
 
+fake = Faker()
+
+customers = []
+
+for i in range(5000):
+    customers.append({
+        "customer_name": fake.name(),
+        "city": fake.city(),
+        "state": fake.state(),
+        "segment": random.choice(
+            ["Consumer", "Corporate", "Small Business"]
+        )
+    })
+
+df = pd.DataFrame(customers)
+
+df.to_csv("customers.csv", index=False)
+
+print(df.head())
+print("Customers generated successfully.")
